8a47dc6bfc4b897e5eb4bf845adf8e4f8c6ac9d2
Alex Speasmaker <alex.speasmaker@gmail.com>
2018-12-04 14:53:09 -0600

    Working on improved run.sh.template

--- rump/npf/run.sh
+++ rump/npf/run.sh
@@ -3,12 +3,9 @@
 IP=10.30.30.134
 APPNAME=rump_server
 
-GUESTARGS="-e RUMPRUN_SYSPROXY=tcp://192.168.1.46:12345"
 
-
-
-#rumprun -S xen -dip -D 1234 -M 256 -N test-rump\
-rumprun -S xen -di -e RUMPRUN_SYSPROXY=tcp://0.0.0.0:8888 -M 512 -N test-rump\
+#rumprun -S xen -di -e RUMPRUN_SYSPROXY=tcp://0.0.0.0:8888 -M 512 -N test-rump\
+rumprun -S xen -dip -D 1234 -e RUMPRUN_SYSPROXY=tcp://0.0.0.0:8888 -M 512 -N test-rump\
     -I xen0,xenif \
 	-W xen0,inet,static,$IP/24 \
 	./$APPNAME.run
